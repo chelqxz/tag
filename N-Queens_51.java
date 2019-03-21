137f994f4aa08d7036f444a96c680cdf95a688eb
chelqxz <48334415+chelqxz@users.noreply.github.com>
2019-03-21 17:59:04 -0400

    Create N-Queens_51.java

---
+++ N-Queens_51.java
@@ -0,0 +1,49 @@
+/** recursive 8 * 7 * 6... (backtrack)
+*/
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        // design the board to do traversal
+        char[][] board = new char[n][n];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                board[i][j] = '.';
+        List<List<String>> res = new ArrayList<List<String>>();
+        dfs(board, 0, res);
+        return res;
+    }
+    
+    private void dfs(char[][] board, int colIndex, List<List<String>> res) {
+        if(colIndex == board.length) {
+            res.add(construct(board));
+            return;
+        }
+        
+        for(int i = 0; i < board.length; i++) {
+            if(validate(board, i, colIndex)) {
+                board[i][colIndex] = 'Q';
+                dfs(board, colIndex + 1, res);
+                board[i][colIndex] = '.'; // backtrack
+            }
+        }
+    }
+    
+    private boolean validate(char[][] board, int x, int y) {
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < y; j++) {
+                if(board[i][j] == 'Q' && (x - y == i - j || x + y == i + j || x == i))// not same row and not same diagonal(both direction)
+                    return false; 
+            }
+        }
+        
+        return true;
+    }
+    // convert board to one one possible configeration
+    private List<String> construct(char[][] board) {
+        List<String> res = new LinkedList<String>();
+        for(int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
+    }
+}
