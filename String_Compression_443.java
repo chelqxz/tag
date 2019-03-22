ef484dee8205669c7b70fa1d18aa1c4d2449f54e
chelqxz <48334415+chelqxz@users.noreply.github.com>
2019-03-22 01:07:31 -0400

    Update String_Compression_443.java

--- String_Compression_443.java
+++ String_Compression_443.java
@@ -1,18 +1,22 @@
+/**
+* count and say
+*/
 class Solution {
     public int compress(char[] chars) {
         int indexAns = 0, index = 0;
         while(index < chars.length){ // end scenario
-            char cur = chars[index];
-            int count = 0;
+            char cur = chars[index]; // current cur
+            int count = 0; // current count
+            // count
             while(index < chars.length && chars[index] == cur){// while so would not miss the last count and say 
                 index++;
                 count++;
             }
-            chars[indexAns++] = cur; 
+            chars[indexAns++] = cur;  // say
             if(count != 1)
                 for(char c : Integer.toString(count).toCharArray())  // use method like this. if not know the exact syntax assume having such a function
                     chars[indexAns++] = c;
         }
-        return indexAns;
+        return indexAns; // simply return the index
     }
 }
