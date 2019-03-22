dbe1d7df31ac58e26933d84034b214f17fcad74f
chelqxz <48334415+chelqxz@users.noreply.github.com>
2019-03-22 06:08:24 -0400

    Update Construct_Binary_Tree_from_String_536.java

--- Construct_Binary_Tree_from_String_536.java
+++ Construct_Binary_Tree_from_String_536.java
@@ -12,6 +12,25 @@
 Method 1 : recursion. discussion里思路和我一样，找到两个child的开始和结束， 然后recursive的解决
 Method 2: stack
 */
+
+
+public TreeNode str2tree(String s) {
+    if (s == null || s.length() == 0) return null;
+    int firstParen = s.indexOf("(");
+    int val = firstParen == -1 ? Integer.parseInt(s) : Integer.parseInt(s.substring(0, firstParen));
+    TreeNode cur = new TreeNode(val);
+    if (firstParen == -1) return cur;
+    int start = firstParen, leftParenCount = 0;
+    for (int i=start;i<s.length();i++) {
+        if (s.charAt(i) == '(') leftParenCount++;
+        else if (s.charAt(i) == ')') leftParenCount--;
+        if (leftParenCount == 0 && start == firstParen) {cur.left = str2tree(s.substring(start+1,i)); start = i+1;}
+        else if (leftParenCount == 0) cur.right = str2tree(s.substring(start+1,i));
+    }
+    return cur;
+}
+
+
 class Solution {
     public TreeNode str2tree(String s) { 
         Stack<TreeNode> stack = new Stack<>();
