687f4177352e9dc1d756a00754eacbc499182d44
chelqxz <48334415+chelqxz@users.noreply.github.com>
2019-03-24 12:37:42 -0400

    Create Valid_Sudoku_36.java

---
+++ Valid_Sudoku_36.java
@@ -0,0 +1,40 @@
+class Solution {
+    // 查each row/ col and 9 cells
+    // can do the three things in one loop <- if need update in these case
+    // though about reduce several pass to one pass
+    public boolean isValidSudoku(char[][] board) {
+        // init data
+        
+        // i-th rows, number n , #times number n appeared in i-th row
+    HashMap<Integer, Integer> [] rows = new HashMap[9];
+    HashMap<Integer, Integer> [] columns = new HashMap[9];
+    HashMap<Integer, Integer> [] boxes = new HashMap[9];
+    for (int i = 0; i < 9; i++) {
+      rows[i] = new HashMap<Integer, Integer>();
+      columns[i] = new HashMap<Integer, Integer>();
+      boxes[i] = new HashMap<Integer, Integer>();
+    }
+
+    // validate a board
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        char num = board[i][j];
+        if (num != '.') {
+          int n = (int)num;
+          int box_index = (i / 3 ) * 3 + j / 3;
+
+          // keep the current cell value
+          rows[i].put(n, rows[i].getOrDefault(n, 0) + 1);
+          columns[j].put(n, columns[j].getOrDefault(n, 0) + 1);
+          boxes[box_index].put(n, boxes[box_index].getOrDefault(n, 0) + 1);
+
+          // check if this value has been already seen before
+          if (rows[i].get(n) > 1 || columns[j].get(n) > 1 || boxes[box_index].get(n) > 1)
+            return false;
+        }
+      }
+    }
+
+    return true;
+    }
+}
